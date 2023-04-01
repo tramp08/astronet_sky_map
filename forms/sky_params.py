91ca63578a4cda1c22eaf0510957b14eb4b52534
tramp08 <oleg@botov.org>
2023-04-01 22:47:46 +0300

    start_web_size_list

--- forms/sky_params.py
+++ forms/sky_params.py
@@ -5,7 +5,14 @@ from wtforms.validators import DataRequired
 
 class SkyForm(FlaskForm):
 
-    xs = IntegerField('размер картинки', default=800)
+    xs = SelectField('размер картинки', choices=[(512, '512'),
+                                                 (640, '640'),
+                                                 (800, '800'),
+                                                 (1024, '1024'),
+                                                 (1152, '1152'),
+                                                 (1280, '1280'),
+                                                 (1600, '1600')
+                                                 ], default=1024)
     theme = IntegerField('тема', default=0)
     longitude = FloatField('долгота', default=-46.307743)
     latitude = FloatField('широта', default=44.269759)
--- main_web.py
+++ main_web.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import datetime
 from pprint import pprint
